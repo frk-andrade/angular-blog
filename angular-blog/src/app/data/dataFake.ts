f38d20519033031bb23c0af89ade2e9211071386
frk-andrade <frkdj25@gmail.com>
2025-05-21 15:56:41 -0300

    Pequenos ajustes

--- angular-blog/src/app/data/dataFake.ts
+++ angular-blog/src/app/data/dataFake.ts
@@ -7,7 +7,19 @@ export const dataFake = [
   },{
     "id":"2",
     "title":"Nova Serie anunciada no Disney+",
-    "description": "Testando as funcionalidades",
+    "description": "Serie mostrará os detalhes que acontecem nos bastidores do multiverso",
     "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ58wdY5fFyX5JeEAQ47l1LDlp_Gf0nNRz4pw&s"
+  },
+  {
+    "id":"3",
+    "title":"Filme conta a história da Viúva Negra",
+    "description": "A vingadora mais durona da Marvel estreia o seu filme solo.",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1WkqwEGh5vlA4NAgNFf1dgSI9nH2OFgpM9A&s"
+  },
+  {
+    "id":"4",
+    "title":"Novo filme do Thor será lançado no início do ano",
+    "description": "Mais uma sequência do filme do Deus do trovão promete lotar as salas de cinema com muita ação",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSmWkCzrbzzBHU9g5KNBquuGjzjHMs87EhtA&s"
   }
 ]
